0cdee41c99c040e1f90f99b836e6a201f6077a5e
Kirsty Devlin <kirsty.leanne.devlin@gmail.com>
2018-08-15 12:22:32 +0100

    adding my completed answers to challenge

--- lib/ordered_jobs.rb
+++ lib/ordered_jobs.rb
@@ -1,4 +1,37 @@
-# challenge 1
-def input_jobs(input)
-  input
+# challenge 1 & 2
+
+class Ordered_jobs
+  def input_jobs(input)
+    input
+  end
+
+# challenge 3 - run jobs in no particular order
+  def run_jobs(jobs)
+    if jobs = {"a" => " ", "b" => " ", "c" => " "}
+      run_jobs.each_key do |key, value| 
+      jobs.push(key)
+      puts key
+      end
+    end
+  end
+
+# challenge 4 - reverse the order of jobs so that c is executed first
+  def reverse_jobs(jobs)
+    if jobs = {"a" => " ", "b" => "c", "c" => " "}
+      jobs.sort { |x, y| x[1]<=>y[1] }
+    end
+  end
+
+# challenge 5
+
+# challenge 6 - throw error
+  def throw_error(jobs)
+    jobs = {"a" => " ", "b" => " ", "c" => "c"}
+      jobs.each do | key, value | 
+        if key == value
+          return "error!"
+        else
+      end
+    end
+  end
 end
--- spec/ordered_jobs_spec.rb
+++ spec/ordered_jobs_spec.rb
@@ -1,15 +1,26 @@
 require_relative '../lib/ordered_jobs'
 
-# challenge 1
-describe 'input_jobs' do
+# challenge 1 - tested and passed
+describe 'ordered_jobs' do
   it 'should receive no letters and return "empty" ' do
     expect(input_jobs("")).to eq ("")
   end
 
-# Challenge 2 -
+# Challenge 2 - tested and passed
 
   it 'should return "a" when entered the letter a' do
-  expect(input_jobs("a")).to eq("a")
+    expect(input_jobs("a")).to eq("a")
   end
 
+# Challenge 3 - failed to test
+  it 'should return 3 jobs in no particular order' do
+    expect(key("a" => "")).to eq ("")
+  end
+# challenge 4 - failed to test
+  it 'should return 3 jobs in reverse order' do
+# challenge 5 -
+
+# challenge 6 - failed to test 
+  it 'should return an error when "value == dependant_value"' do
+  expect (jobs("c" == "c")).to raise_error
 end
