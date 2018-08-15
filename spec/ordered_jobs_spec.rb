afc3fa8328548eb75b6b2661282536011b20d2af
Kirsty Devlin <kirsty.leanne.devlin@gmail.com>
2018-08-15 12:03:47 +0100

    refactored and added notes file

--- spec/ordered_jobs_spec.rb
+++ spec/ordered_jobs_spec.rb
@@ -5,11 +5,11 @@ describe 'input_jobs' do
   it 'should receive no letters and return "empty" ' do
     expect(input_jobs("")).to eq ("")
   end
-end
 
 # Challenge 2 -
-describe 'input_jobs' do
-it 'should return "a" when entered the letter a' do
+
+  it 'should return "a" when entered the letter a' do
   expect(input_jobs("a")).to eq("a")
   end
+
 end
